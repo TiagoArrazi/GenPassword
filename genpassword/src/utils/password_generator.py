00bbde447f1799031bc8a08e4db204521fbcc70c
Tiago Arrazi <tiago_arrazi98@outlook.com>
2019-08-27 09:50:11 -0300

    Novo método generate_unbreakable com SHA512

--- genpassword/src/utils/password_generator.py
+++ genpassword/src/utils/password_generator.py
@@ -1,17 +1,28 @@
 from random import choice, randint
 import string
+import hashlib
 
 
 class Generator:
 
     @classmethod
+    def generate_unbreakable(cls):
+        chars = [string.ascii_lowercase,
+                 string.ascii_uppercase,
+                 string.digits,
+                 string.punctuation]
+
+        pass_length = randint(15, 30)
+
+        return hashlib.sha512(''.join([choice(choice(chars)) for i in range(pass_length)]).encode('utf-8')).hexdigest()
+
+    @classmethod
     def generate(cls):
         chars = [string.ascii_lowercase,
                  string.ascii_uppercase,
-                 string.digits]
+                 string.digits,
+                 string.punctuation]
 
-        pass_length = randint(10, 20)
+        pass_length = randint(15, 30)
 
         return ''.join([choice(choice(chars)) for i in range(pass_length)])
-
-
